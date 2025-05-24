0076d64616d99fc4e130661557ae6aa78b814b7f
Saiful Islam <76849761+saifulIslamSourceCode@users.noreply.github.com>
2025-05-24 17:50:32 +0600

    Todo App using Angular, Scss & Bootstrap
    Making projects for learning angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoMainComponent } from './todo-main/todo-main.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoMainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo';
+
 }
---
+++ src/app/todo-main/todo-main.component.ts
@@ -0,0 +1,51 @@
+import { NgClass, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface TodoItem {
+  id: number,
+  task: string,
+  completed: boolean
+}
+
+@Component({
+  selector: 'app-todo-main',
+  imports: [FormsModule, NgFor, NgClass],
+  templateUrl: './todo-main.component.html',
+  styleUrl: './todo-main.component.scss'
+})
+export class TodoMainComponent {
+
+    todoList: TodoItem [] = [];
+    newTask: string = "";
+
+    //geting new task and assigned in newTodoItem
+    addTask(): void {
+      if(this.newTask !== '') {
+        const newTodoItem: TodoItem = {
+          id: Date.now(),
+          task: this.newTask,
+          completed: false
+        }
+
+        //add new added item in the todo list array
+        this.todoList.push(newTodoItem);
+        this.newTask = ''
+      }
+    }
+
+  //complete the todo item fn
+  toggleCompleted(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed;
+    // label.style.textDecoration = this.todoList[index].completed ? 'underline': 'none';
+
+    // console.log(this.todoList[index])
+    
+  }
+
+  //delete btn event
+  deleteTask(index: number) {
+    this.todoList.splice(index, 1); 
+  }
+
+}
